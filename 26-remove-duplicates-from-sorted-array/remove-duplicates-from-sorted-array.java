cb6868b4ec7194d45a57090c39dc9f1fcf0d638a
Arushi Pundir <62982101+ArushiPundir@users.noreply.github.com>
2026-06-13 12:33:44 +0530

    Time: 1 ms (77.18%) | Memory: 46.6 MB (61.20%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int count = 0;
+        for (int i =1; i<nums.length; i++){
+            if(nums[i]!=nums[i-1]){
+                count++;
+                nums[count] = nums[i];
+            }
+        }
+        return count+1;
+    }
+}
