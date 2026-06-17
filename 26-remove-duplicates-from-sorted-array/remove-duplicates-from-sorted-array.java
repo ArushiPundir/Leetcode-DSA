f83f080ef8bc1804e15630f3f8b18c18a66d44a1
Arushi Pundir <62982101+ArushiPundir@users.noreply.github.com>
2026-06-17 07:57:37 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (41.70%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -3,13 +3,14 @@ class Solution {
         if(nums.length == 0){
             return 0;
         }
-        int count =0;
+        int count =1;
         for(int i=1;i< nums.length;i++){
             if(nums[i]!=nums[i-1]){
-                count++;
                 nums[count]=nums[i];
+                count++;
+                
             }
         }
-        return count+1;
+        return count;
     }
 }
