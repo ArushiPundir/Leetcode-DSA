4a8b53e4d2a8ecfbe5e232f389cda31b154f706a
Arushi Pundir <62982101+ArushiPundir@users.noreply.github.com>
2026-07-01 18:16:54 +0530

    Time: 3 ms (72.70%) | Memory: 52.8 MB (21.40%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,16 +1,45 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer,Integer> map = new HashMap<>();
+        int el1 = Integer.MIN_VALUE;
+        int el2 = Integer.MIN_VALUE;
+        int cnt1 = 0;
+        int cnt2 = 0;
         List<Integer> list = new ArrayList<>();
+        for(int i =0;i<nums.length;i++){
+            if(cnt1 == 0 && el2 != nums[i]){
+                el1 = nums[i];
+                cnt1 =1;
+            }
+            else if(cnt2 == 0 && el1 != nums[i]){
+                el2 = nums[i];
+                cnt2 =1;
+            }
+            else if(el1 == nums[i]){
+                cnt1++;
+            }
+            else if(el2 == nums[i]){
+                cnt2++;
+            }
+            else{
+                cnt1--; cnt2--;
+            }
+        }
+        //manual check
+        cnt1 = 0; cnt2= 0;
+        for(int i =0;i<nums.length;i++){
+            if(el1 == nums[i]){
+                cnt1++;
+            }
+            if(el2 == nums[i]){
+                cnt2++;
+            }
+        }
         int mini = (nums.length)/3;
-        for(int i =0; i<nums.length;i++){
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-            if(map.get(nums[i]) > mini && !list.contains(nums[i])){
-                list.add(nums[i]);
-            }
-            if(list.size()==2){
-                break;
-            }
+        if(cnt1>mini) {
+            list.add(el1);
+        }
+        if(cnt2>mini){
+            list.add(el2);
         }
         return list;
     }
